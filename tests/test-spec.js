c11442f449d59fadca63d6e68fb3412069924a9d
Helen Yi <helenhyi@gmail.com>
2017-10-25 19:51:44 -0700

    (feat) set up mock data generation

---
+++ tests/test-spec.js
@@ -0,0 +1,75 @@
+var mysql = require('mysql');
+var request = require('request'); 
+var expect = require('chai').expect;
+
+describe('Persistent Inventory Server', function() {
+  var dbConnection;
+
+  beforeEach(function(done) {
+    dbConnection = mysql.createConnection({
+      user: 'root',
+      password: '',
+      database: 'inventory'
+    });
+    dbConnection.connect();
+
+       var tablename = "category"; // TODO: fill this out
+
+    /* Empty the db table before each test so that multiple tests
+     * (or repeated runs of the tests) won't screw each other up: */
+    dbConnection.query('truncate ' + tablename, done);
+  });
+
+  afterEach(function() {
+    dbConnection.end();
+  });
+
+  it('Should insert posted category to the DB', function(done) {
+    // Post a category
+    request({
+      method: 'POST',
+      uri: 'http://127.0.0.1:3000/addCategory',
+      json: [ 5000, 'test category', 1 ]
+    }, function () {
+        // Now if we look in the database, we should find the
+        // posted message there.
+
+        // TODO: You might have to change this test to get all the data from
+        // your message table, since this is schema-dependent.
+        var queryString = 'SELECT * FROM category';
+        var queryArgs = [];
+
+        dbConnection.query(queryString, queryArgs, function(err, results) {
+          // Should have one result:
+          expect(results.length).to.equal(1);
+
+          // TODO: If you don't have a column named text, change this test.
+          // expect(results[0].text).to.equal('In mercy\'s name, three days is all I need.');
+
+          done();
+      });
+    });
+  });
+
+  xit('Should output all messages from the DB', function(done) {
+    // Let's insert a message into the db
+       var queryString = "";
+       var queryArgs = [];
+    // TODO - The exact query string and query args to use
+    // here depend on the schema you design, so I'll leave
+    // them up to you. */
+
+    dbConnection.query(queryString, queryArgs, function(err) {
+      if (err) { throw err; }
+
+      // Now query the Node chat server and see if it returns
+      // the message we just inserted:
+      request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
+        var messageLog = JSON.parse(body);
+        expect(messageLog[0].text).to.equal('Men like you can never change!');
+        expect(messageLog[0].roomname).to.equal('main');
+        done();
+      });
+    });
+  });
+});
